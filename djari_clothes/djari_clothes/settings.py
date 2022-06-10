1e1da4b3543c8ef5f313bd0054dec5a7ae53ddb2
LuisAlexisSalazar <kirito_SWO@outlook.com>
2022-06-10 09:16:55 -0500

    Resolviendo algunos problemas

--- djari_clothes/djari_clothes/settings.py
+++ djari_clothes/djari_clothes/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 # *Ubicar las Imagenes
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR + '/media/'
+
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
